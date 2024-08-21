192aab3bce35caac7a50ff719d6b46fa0657ea18
helio2323 <helio_lima@outlook.com.br>
2024-08-21 17:09:05 -0300

    ajustes

--- backend/route.py
+++ backend/route.py
@@ -1,61 +1,80 @@
-from flask import Flask, request, g
-import sqlite3
+from flask import Flask, render_template, redirect, url_for, request
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-from src.services.bot import get_products_site, login, update_all_products
+import requests
+import time
+
+from src.services.bot import get_products_site, update_all_products
 from src.models.Sqclass import Sqclass
 from src.services.Scraper import Navegador
-from src.services.save_data import save_catalog
+from src.services.save_data import save_catalog, generate_pdf_from_db
 
-import time
+app = Flask(__name__)
 
 bd = Sqclass()
 
-app = Flask(__name__)
-def get_db():
-    if 'db' not in g:
-        db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../backend/src/database/database.db'))
-        g.db = sqlite3.connect(db_path, check_same_thread=False)
-        g.db.cursor()  # Initialize cursor
-    return g.db
+@app.route('/')
+def index():
+    return render_template('menu.html')
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = g.pop('db', None)
-    if db is not None:
-        db.close()
+@app.route('/list_products')
+def list_products():
+    listas = bd.get_product_list()
+    if listas:
+        listas_text = "\n".join([f'ID: {list_item[0]} | LISTA: {list_item[1]}' for list_item in listas])
+    else:
+        listas_text = "Nenhuma lista de produtos encontrada."
+    return render_template('result.html', result=listas_text)
 
-@app.route('/update/<name>')
-def update_products(name):
-    db = get_db()
-    cursor = db.cursor()
-    
-    name = int(name)
-    
-    cursor.execute("SELECT * FROM product_list")
-    lista = cursor.fetchall()
-    
-    bot = Navegador()
-    for list_item in lista:
-        if list_item[0] == name:
-            url = list_item[2]
-            LIST_ID = list_item[0]
-            get_products_site(LIST_ID, url, bot)
-            print(f"Lista de produtos {LIST_ID} atualizada com sucesso.")
-            bot.click('XPATH', '/html/body/nav/div/div[4]/div[1]')
-            time.sleep(5)
-            bot.close()
-            break
-    
-    return f'Hello, {name}!'
-
-@app.route('/allproducts')
-def all_products():
+@app.route('/update_all_products')
+def update_all_products_route():
     bot = Navegador()
     update_all_products(bot)
-    
-    return 'All products updated successfully.'
+    return render_template('result.html', result="Todos os produtos foram atualizados.")
+
+@app.route('/update_single_list', methods=['GET'])
+def update_single_list():
+    lista_id = request.args.get('lista_id', type=int)
+    if not lista_id:
+        return render_template('result.html', result="ID da lista não fornecido.")
+    try:
+        lista = bd.get_product_list()
+        bot = Navegador()
+        for list_item in lista:
+            if list_item[0] == lista_id:
+                url = list_item[2]
+                LIST_ID = list_item[0]
+                get_products_site(LIST_ID, url, bot)
+                bot.click('XPATH', '/html/body/nav/div/div[4]/div[1]')
+                time.sleep(5)
+                bot.close()
+                return render_template('result.html', result=f"Lista de produtos {LIST_ID} atualizada com sucesso.")
+    except Exception as e:
+        return render_template('result.html', result=f"Erro ao atualizar a lista de produtos: {e}")
+    return render_template('result.html', result="ID da lista não encontrado.")
+
+@app.route('/generate_catalog', methods=['GET'])
+def generate_catalog():
+    lista_id = request.args.get('lista_id', type=int)
+    if not lista_id:
+        return render_template('result.html', result="ID da lista não fornecido.")
+    save_catalog(lista_id)
+    prod_list = bd.get_product_list()
+    for prod in prod_list:
+        if prod[0] == lista_id:
+            name_list = prod[1]
+            break
+    generate_pdf_from_db(lista_id, f'./Catalogo/{lista_id}-{name_list}.html', f'./Catalogo/{lista_id}-{name_list}.pdf')
+    return render_template('result.html', result=f"Catálogo para a lista {lista_id} gerado com sucesso.")
+
+@app.route('/set_all_list_false')
+def set_all_list_false():
+    bd.set_all_list_false()
+    return render_template('result.html', result="Todas as listas de produtos foram atualizadas como False.")
+
+@app.route('/exit')
+def exit_app():
+    return render_template('result.html', result="Saindo...")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/src/models/Sqclass.py
+++ backend/src/models/Sqclass.py
@@ -3,169 +3,162 @@ import os
 
 class Sqclass:
     def __init__(self):
-        db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../database/database.db'))
-        self.conn = sqlite3.connect(db_path)
-        self.cursor = self.conn.cursor()
-        self.create_table_lists()
-        self.create_table_products()
-        self.create_table_products_infos()
-        
-    def close(self):
-        self.conn.close()
-    
-    def create_table_lists(self):
-        self.cursor.execute(""" 
-                            CREATE TABLE IF NOT EXISTS product_list (
-                                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                name TEXT,
-                                url TEXT,
-                                updated BOOLEAN DEFAULT 0
-                            )
-                            """)
-        self.conn.commit()
-        
-    
-    def create_table_products(self):
-        self.cursor.execute(""" 
-                            CREATE TABLE IF NOT EXISTS products (
-                                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                title TEXT,
-                                photo_src TEXT,
-                                product_site_id TEXT,
-                                id_list INTEGER,
-                                image BLOB,
-                                updated BOOLEAN DEFAULT 0,
-                                FOREIGN KEY(id_list) REFERENCES product_list(id)
-                            )
-                            """)
-        self.conn.commit()
-    
-    def create_table_products_infos(self):
-        self.cursor.execute(""" 
-                            CREATE TABLE IF NOT EXISTS products_infos (
-                                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                site_option_id TEXT,
-                                size TEXT,
-                                price_web REAL,
-                                price_b2b REAL,
-                                id_product INTEGER,
-                                FOREIGN KEY(id_product) REFERENCES products(id)
-                            )
-                            """)
-        self.conn.commit()
+        # O caminho do banco de dados
+        self.db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../database/database.db'))
+        self.create_tables()
 
+    def create_tables(self):
+        # Criação das tabelas
+        with self.get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(""" 
+                CREATE TABLE IF NOT EXISTS product_list (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT,
+                    url TEXT,
+                    updated BOOLEAN DEFAULT 0
+                )
+            """)
+            cursor.execute(""" 
+                CREATE TABLE IF NOT EXISTS products (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT,
+                    photo_src TEXT,
+                    product_site_id TEXT,
+                    id_list INTEGER,
+                    image BLOB,
+                    updated BOOLEAN DEFAULT 0,
+                    FOREIGN KEY(id_list) REFERENCES product_list(id)
+                )
+            """)
+            cursor.execute(""" 
+                CREATE TABLE IF NOT EXISTS products_infos (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    site_option_id TEXT,
+                    size TEXT,
+                    price_web REAL,
+                    price_b2b REAL,
+                    id_product INTEGER,
+                    FOREIGN KEY(id_product) REFERENCES products(id)
+                )
+            """)
+
+    def get_db_connection(self):
+        """Cria e retorna uma nova conexão com o banco de dados."""
+        connection = sqlite3.connect(self.db_path, check_same_thread=False)
+        connection.row_factory = sqlite3.Row
+        return connection
+
+    def close(self, conn):
+        """Fecha a conexão com o banco de dados."""
+        conn.close()
     
     def insert_into_product_list(self, name, url):
-        self.cursor.execute("""
-                            INSERT INTO product_list (name, url)
-                            VALUES (?, ?)
-                            """, (name, url))
-        self.conn.commit()
-        return self.cursor.lastrowid  # Retorna o ID do registro inserido
-    
+        with self.get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                INSERT INTO product_list (name, url)
+                VALUES (?, ?)
+            """, (name, url))
+            conn.commit()
+            return cursor.lastrowid
     
     def get_product_list(self):
-        self.cursor.execute("SELECT * FROM product_list")
-        return self.cursor.fetchall()
+        with self.get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM product_list")
+            return cursor.fetchall()
     
     def update_status_list(self, id_list):
-        self.cursor.execute("UPDATE product_list SET updated = 1 WHERE id = ?", (id_list,))
-        self.conn.commit()
+        with self.get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("UPDATE product_list SET updated = 1 WHERE id = ?", (id_list,))
+            conn.commit()
         
     def set_all_list_false(self):
-        self.cursor.execute("UPDATE product_list SET updated = 0")
-        self.conn.commit()
+        with self.get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("UPDATE product_list SET updated = 0")
+            conn.commit()
     
-    # Pucha os dados da tabela lists e junta com os dados da tabela 'products' + 'products_infos'
     def get_products(self, id_list):
-        self.cursor.execute("""
-                            SELECT products.id, products.title, products.photo_src, products.product_site_id, products.image, products_infos.site_option_id, products_infos.size, products_infos.price_web, products_infos.price_b2b
-                            FROM products
-                            INNER JOIN products_infos
-                            ON products.id = products_infos.id_product
-                            WHERE products.id_list = ?
-                            """, (id_list,))
-        return self.cursor.fetchall()
+        with self.get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT products.id, products.title, products.photo_src, products.product_site_id, products.image, products_infos.site_option_id, products_infos.size, products_infos.price_web, products_infos.price_b2b
+                FROM products
+                INNER JOIN products_infos
+                ON products.id = products_infos.id_product
+                WHERE products.id_list = ?
+            """, (id_list,))
+            return cursor.fetchall()
     
     def get_prod_xlsx(self, id_list):
-        self.cursor.execute("""
-                            SELECT products.id, products.title, products.image, products_infos.price_b2b
-                            FROM products
-                            INNER JOIN products_infos
-                            ON products.id = products_infos.id_product
-                            WHERE products.id_list = ?
-                            """, (id_list,))
-        return self.cursor.fetchall()
+        with self.get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT products.id, products.title, products.image, products_infos.price_b2b
+                FROM products
+                INNER JOIN products_infos
+                ON products.id = products_infos.id_product
+                WHERE products.id_list = ?
+            """, (id_list,))
+            return cursor.fetchall()
 
     def get_prod_pdf(self, id_list):
-        self.cursor.execute("""
-                            SELECT products.id, products.title, products.photo_src, products_infos.price_b2b
-                            FROM products
-                            INNER JOIN products_infos
-                            ON products.id = products_infos.id_product
-                            WHERE products.id_list = ?
-                            """, (id_list,))
-        return self.cursor.fetchall()
-
-
+        with self.get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT products.id, products.title, products.photo_src, products_infos.price_b2b
+                FROM products
+                INNER JOIN products_infos
+                ON products.id = products_infos.id_product
+                WHERE products.id_list = ?
+            """, (id_list,))
+            return cursor.fetchall()
     
-    # Método para inserir dados na tabela products verifica se o produto ja existe comparando o product_site_id caso exista faz o update
-# Método para inserir ou atualizar dados na tabela 'products'
     def insert_or_update_product(self, title, photo_src, product_site_id, id_list, image_binary, updated=0):
-        # Verificar se o produto já existe
-        self.cursor.execute("SELECT id FROM products WHERE product_site_id = ?", (product_site_id,))
-        existing_product = self.cursor.fetchone()
-        
-        if existing_product:
-            product_id = existing_product[0]
-            # Se o produto existir, faça a atualização
-            self.cursor.execute("""
-                UPDATE products
-                SET title = ?, photo_src = ?, id_list = ?, image = ?, updated = ?
-                WHERE product_site_id = ?
-            """, (title, photo_src, id_list, image_binary, updated, product_site_id))
-        else:
-            # Se o produto não existir, faça a inserção
-            self.cursor.execute("""
-                INSERT INTO products (title, photo_src, product_site_id, id_list, image, updated)
-                VALUES (?, ?, ?, ?, ?, ?)
-            """, (title, photo_src, product_site_id, id_list, image_binary, updated))
-            product_id = self.cursor.lastrowid
-        
-        # Confirma a transação no banco de dados
-        self.conn.commit()
-        
-        # Retorna o ID do produto existente ou recém-inserido
-        return product_id
+        with self.get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT id FROM products WHERE product_site_id = ?", (product_site_id,))
+            existing_product = cursor.fetchone()
+            
+            if existing_product:
+                product_id = existing_product[0]
+                cursor.execute("""
+                    UPDATE products
+                    SET title = ?, photo_src = ?, id_list = ?, image = ?, updated = ?
+                    WHERE product_site_id = ?
+                """, (title, photo_src, id_list, image_binary, updated, product_site_id))
+            else:
+                cursor.execute("""
+                    INSERT INTO products (title, photo_src, product_site_id, id_list, image, updated)
+                    VALUES (?, ?, ?, ?, ?, ?)
+                """, (title, photo_src, product_site_id, id_list, image_binary, updated))
+                product_id = cursor.lastrowid
+            
+            conn.commit()
+            return product_id
 
-
-
-    # Método para inserir dados na tabela products_infos verifica se as informações ja existe comparando o site_option_id caso exista faz o update
     def insert_or_update_products_infos(self, site_option_id, size, price_web, price_b2b, id_product):
-        # Verificar se as informações do produto já existem
-        self.cursor.execute("SELECT id FROM products_infos WHERE site_option_id = ?", (site_option_id,))
-        existing_info = self.cursor.fetchone()
-        
-        if existing_info:
-            info_id = existing_info[0]
-            # Se as informações existirem, faça a atualização
-            self.cursor.execute("""
-                UPDATE products_infos
-                SET size = ?, price_web = ?, price_b2b = ?, id_product = ?
-                WHERE site_option_id = ?
-            """, (size, price_web, price_b2b, id_product, site_option_id))
-        else:
-            # Se as informações não existirem, faça a inserção
-            self.cursor.execute("""
-                INSERT INTO products_infos (site_option_id, size, price_web, price_b2b, id_product)
-                VALUES (?, ?, ?, ?, ?)
-            """, (site_option_id, size, price_web, price_b2b, id_product))
-            info_id = self.cursor.lastrowid
-        
-        # Confirma a transação no banco de dados
-        self.conn.commit()
-        
-        # Retorna o ID do registro existente ou recém-inserido
-        return info_id
-
-    
+        with self.get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT id FROM products_infos WHERE site_option_id = ?", (site_option_id,))
+            existing_info = cursor.fetchone()
+            
+            if existing_info:
+                info_id = existing_info[0]
+                cursor.execute("""
+                    UPDATE products_infos
+                    SET size = ?, price_web = ?, price_b2b = ?, id_product = ?
+                    WHERE site_option_id = ?
+                """, (size, price_web, price_b2b, id_product, site_option_id))
+            else:
+                cursor.execute("""
+                    INSERT INTO products_infos (site_option_id, size, price_web, price_b2b, id_product)
+                    VALUES (?, ?, ?, ?, ?)
+                """, (site_option_id, size, price_web, price_b2b, id_product))
+                info_id = cursor.lastrowid
+            
+            conn.commit()
+            return info_id
