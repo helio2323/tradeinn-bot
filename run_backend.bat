23a2a06ee89f82c41f7113b3025a66baa3f92587
Helio Fernandes dos Reis Lima <90978646+helio2323@users.noreply.github.com>
2024-11-15 19:10:09 -0300

    Update run_backend.bat

--- run_backend.bat
+++ run_backend.bat
@@ -2,7 +2,7 @@
 :: Criar ambiente virtual
 py -m venv venv
 
-:: Verifique se o ambiente virtual está ativado corretamente
+:: Verifique se o ambiente virtual foi criado corretamente
 if not exist venv\Scripts\activate (
     echo Ambiente virtual não encontrado. Verifique o caminho.
     pause
@@ -12,16 +12,25 @@ if not exist venv\Scripts\activate (
 :: Ativar ambiente virtual
 call venv\Scripts\activate
 
-:: Instalar dependências
+:: Atualizar pip para evitar problemas de compatibilidade
+call python -m pip install --upgrade pip
+
+:: Instalar dependências do projeto
 call pip install -r requirements.txt
 
-:: Mudar para o diretório backend
+:: Atualizar e reinstalar Pillow para corrigir possíveis problemas de compatibilidade
+call pip install --upgrade pillow
+call pip uninstall pillow -y
+call pip install pillow
+
+:: Verificar se o diretório backend existe
 if not exist backend (
     echo Diretório backend não encontrado. Verifique o caminho.
     pause
     exit /b 1
 )
 
+:: Mudar para o diretório backend
 cd backend
 
 :: Executar o script Python
